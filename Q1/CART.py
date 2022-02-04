f4174ff922adc92cb84e807d39abdd0884fd6ad7
Jordan Santos <51087796+jordan080@users.noreply.github.com>
2022-02-04 11:05:47 -0300

    Add files via upload

---
+++ Q1/CART.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+from sklearn.preprocessing import OrdinalEncoder
+
+dataset = pd.read_csv('database.csv')
+dataset = dataset.loc[:, dataset.columns != 'Exemplo']
+enc = OrdinalEncoder()
+dataset = enc.fit_transform(dataset)
+
+X = dataset[:, :-1]
+print(X[0])
+print(X[1])
+print(X[2])
+print(X[3])
+
+y = dataset[:, -1]
+
+clf = DecisionTreeClassifier(random_state = 0)
+clf = clf.fit(X, y)
+
+plot_tree(clf)
+plt.show()
+
