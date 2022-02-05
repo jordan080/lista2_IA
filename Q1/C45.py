9d0fdcb2490cabcb36c0304dbd2ffa965e10001f
priscilat <pts@ic.ufal.br>
2022-02-05 10:10:43 -0300

    C4.5 gerando arvore de decisao

--- Q1/C45.py
+++ Q1/C45.py
@@ -4,13 +4,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
+from sklearn.preprocessing import OrdinalEncoder
 
 dataset = pd.read_csv("Q1/database.csv")
 dataset = dataset.loc[:, dataset.columns != 'Exemplo']
 
-config = {'algorithm' : 'CART'}
+config = {'algorithm' : 'C4.5'}
 
-model = chef.fit(dataset.copy(),  config = config, target_label = 'Risco')
+model = chef.fit(dataset.copy(),  config = {}, target_label = 'Risco', validation_df = None)
 
-print(model)
+features = ['História de Credito',	'Dívida', 'Garantia', 'Renda']
+X = dataset[features]
 
+features2 = ['Risco']
+Y = dataset[features2]
+
+enc = OrdinalEncoder()
+a = enc.fit_transform(X)
+b = enc.fit_transform(Y)
+
+clf = DecisionTreeClassifier(random_state = 0)
+clf = clf.fit(a, b)
+
+plot_tree(clf)
+plt.show()
--- Q1/outputs/rules/rules.py
+++ Q1/outputs/rules/rules.py
@@ -1,43 +1,48 @@
-def findDecision(obj): #obj[0]: Exemplo, obj[1]: História de Credito, obj[2]: Dívida, obj[3]: Garantia, obj[4]: Renda
-	# {"feature": "Exemplo", "instances": 20, "metric_value": 1.7884, "depth": 1}
-	if obj[0] == 'E1':
-		return 'Alto'
-	elif obj[0] == 'E2':
-		return 'Alto'
-	elif obj[0] == 'E19':
-		return 'Moderado'
-	elif obj[0] == 'E18':
-		return 'Baixo'
-	elif obj[0] == 'E17':
-		return 'Baixo'
-	elif obj[0] == 'E16':
-		return 'Alta'
-	elif obj[0] == 'E15':
-		return 'Moderado'
-	elif obj[0] == 'E14':
-		return 'Alto'
-	elif obj[0] == 'E13':
-		return 'Baixo'
-	elif obj[0] == 'E12':
-		return 'Moderado'
-	elif obj[0] == 'E11':
-		return 'Alto'
-	elif obj[0] == 'E10':
-		return 'Baixo'
-	elif obj[0] == 'E9':
-		return 'Baixo'
-	elif obj[0] == 'E8':
-		return 'Moderado'
-	elif obj[0] == 'E7':
-		return 'Alto'
-	elif obj[0] == 'E6':
-		return 'Baixo'
-	elif obj[0] == 'E5':
-		return 'Baixo'
-	elif obj[0] == 'E4':
-		return 'Alto'
-	elif obj[0] == 'E3':
-		return 'Moderado'
-	elif obj[0] == 'E20':
-		return 'Moderado'
+def findDecision(obj): #obj[0]: História de Credito, obj[1]: Dívida, obj[2]: Garantia, obj[3]: Renda
+	# {"feature": "Renda", "instances": 20, "metric_value": 1.7884, "depth": 1}
+	if obj[3] == 'Acima_de_$35k':
+		# {"feature": "Hist\u00f3ria de Credito", "instances": 9, "metric_value": 1.2244, "depth": 2}
+		if obj[0] == 'Boa':
+			return 'Baixo'
+		elif obj[0] == 'Desconhecido':
+			# {"feature": "D\u00edvida", "instances": 3, "metric_value": 0.9183, "depth": 3}
+			if obj[1] == 'Baixa':
+				return 'Baixo'
+			elif obj[1] == 'Alta':
+				return 'Alta'
+			else: return 'Alta'
+		elif obj[0] == 'Ruim':
+			return 'Moderado'
+		else: return 'Moderado'
+	elif obj[3] == '$0_a_$15k':
+		# {"feature": "Garantia", "instances": 6, "metric_value": 1.2516, "depth": 2}
+		if obj[2] == 'Nenhuma':
+			return 'Alto'
+		elif obj[2] == 'Adequada':
+			# {"feature": "Hist\u00f3ria de Credito", "instances": 2, "metric_value": 1.0, "depth": 3}
+			if obj[0] == 'Boa':
+				return 'Baixo'
+			elif obj[0] == 'Ruim':
+				return 'Moderado'
+			else: return 'Moderado'
+		else: return 'Baixo'
+	elif obj[3] == '$15_a_$35k':
+		# {"feature": "Hist\u00f3ria de Credito", "instances": 5, "metric_value": 0.971, "depth": 2}
+		if obj[0] == 'Desconhecido':
+			# {"feature": "D\u00edvida", "instances": 3, "metric_value": 0.9183, "depth": 3}
+			if obj[1] == 'Alta':
+				# {"feature": "Garantia", "instances": 2, "metric_value": 1.0, "depth": 4}
+				if obj[2] == 'Nenhuma':
+					return 'Alto'
+				elif obj[2] == 'Adequada':
+					return 'Moderado'
+				else: return 'Moderado'
+			elif obj[1] == 'Baixa':
+				return 'Moderado'
+			else: return 'Moderado'
+		elif obj[0] == 'Boa':
+			return 'Moderado'
+		elif obj[0] == 'Ruim':
+			return 'Alto'
+		else: return 'Alto'
 	else: return 'Moderado'
